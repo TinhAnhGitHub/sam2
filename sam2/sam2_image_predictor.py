a8a9f6252637229011ede282de517b400aec4a81
Tinh Anh <anh.nguyennhu2306@hcmut.edu.vn>
2024-10-12 21:16:03 +0700

    debug: printing the vision feats in set_image() function, SAM2ImagePredictor

--- sam2/sam2_image_predictor.py
+++ sam2/sam2_image_predictor.py
@@ -114,8 +114,13 @@ class SAM2ImagePredictor:
             len(input_image.shape) == 4 and input_image.shape[1] == 3
         ), f"input_image must be of size 1x3xHxW, got {input_image.shape}"
         logging.info("Computing image embeddings for the provided image...")
+
         backbone_out = self.model.forward_image(input_image)
         _, vision_feats, _, _ = self.model._prepare_backbone_features(backbone_out)
+
+        ## DEBUG
+        print("Vision Feats shape: ", vision_feats.shape)
+
         # Add no_mem_embed, which is added to the lowest rest feat. map during training on videos
         if self.model.directly_add_no_mem_embed:
             vision_feats[-1] = vision_feats[-1] + self.model.no_mem_embed
